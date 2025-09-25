2bad2721de8f8d3d50902bec804c4beca3417454
Gonzalo <gjaimeguinazu@gmail.com>
2025-09-25 08:50:22 -0300

    implementar cambios para el enrutado de la app

--- superhard-frontend/src/App.jsx
+++ superhard-frontend/src/App.jsx
@@ -3,17 +3,11 @@ import Header from "./components/Header";
 import Carousel from "./components/Carousel";
 import "./index.css";
 import "./App.css";
+import AppRouter from "./routes/AppRouter";
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen w-screen bg-neutral-900 text-white">
-      <Header />
-      <main className="flex-1 p-5">
-        <Carousel />
-
-        <h2>Contenido principal de la página</h2>
-      </main>
-    </div>
+    <AppRouter />
   );
 }
 
