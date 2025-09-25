fab4dc180ac709cfc4c216e034a7c9427f93f71c
Gonzalo <gjaimeguinazu@gmail.com>
2025-09-25 08:49:03 -0300

    pagina principal en un componente aparte

---
+++ superhard-frontend/src/components/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Carousel from '../Carousel'
+import Header from '../Header'
+
+const Home = () => {
+  return (
+    <div className="flex flex-col min-h-screen w-screen bg-neutral-900 text-white">
+      <Header />
+      <main className="flex-1 p-5">
+        <Carousel />
+
+        <h2>Contenido principal de la página</h2>
+      </main>
+    </div>
+  )
+}
+
+export default Home
