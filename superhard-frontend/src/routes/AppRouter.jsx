712bafcf85e9818671d0e62966268450a004d658
Gonzalo <gjaimeguinazu@gmail.com>
2025-09-25 10:50:21 -0300

    seteando el componente Header como layout para la ruta home

--- superhard-frontend/src/routes/AppRouter.jsx
+++ superhard-frontend/src/routes/AppRouter.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from '../components/pages/Home';
 import Login from '../components/pages/Login';
+import Header from '../components/Header';
 
 
 const AppRouter = () => {
   return (
     <Router>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-        </Routes>
+      <Routes>
+        <Route element={<Header />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </Router>
   )
 }
