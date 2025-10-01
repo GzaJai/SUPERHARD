3ffac700b06d99741fab72fd535b2bd22ba0842b
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-10-01 15:30:39 -0300

    creando ruta para detalles de producto con path variables

--- superhard-frontend/src/routes/AppRouter.jsx
+++ superhard-frontend/src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from '../components/pages/Home';
 import Login from '../components/pages/Login';
 import Header from '../components/Header';
+import ProductDetails from '../components/pages/ProductDetails';
 
 
 const AppRouter = () => {
@@ -11,6 +12,7 @@ const AppRouter = () => {
       <Routes>
         <Route element={<Header />}>
           <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />} />
         </Route>
         <Route path='/login' element={<Login />} />
       </Routes>
