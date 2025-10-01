93d8df91440e4b8fdb463efae06380e087eb0820
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-10-01 13:48:20 -0300

    implementando componente banner

--- superhard-frontend/src/components/pages/Home.jsx
+++ superhard-frontend/src/components/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Carousel from '../Carousel'
-import Header from '../Header'
+import Banner from '../Banner'
+import ImagenPrincipal from "../../assets/images/Imagen principal.png";
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen w-screen bg-neutral-900 text-white">
+    <div className="flex flex-col h-dvh bg-neutral-900 text-white">
       <main className="flex-1 p-5">
+        <Banner imgPath={ImagenPrincipal} />
         <Carousel />
 
         <h2>Contenido principal de la página</h2>
