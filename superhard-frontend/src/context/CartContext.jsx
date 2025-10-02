39b2ef6c1cf7a537b3fd083c131e94dcb241b915
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-10-01 22:03:39 -0300

    agregando contador de items en el carrito

--- superhard-frontend/src/context/CartContext.jsx
+++ superhard-frontend/src/context/CartContext.jsx
@@ -1,20 +1,25 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 export const CartContext = createContext();
 
 export function CartProvider({ children }) {
     const [cart, setCart] = useState([]);
+    const [cartQuantity, setCartQuantity] = useState(0);
+
+    useEffect(() => {
+        setCartQuantity(cart.length)
+    }, [cart])
 
     const addToCart = (product) => {
         setCart((prev) => [...prev, product]);
     };
-
+    
     const removeFromCart = () => {
         setCart((prev) => prev.filter(item => item.id !== id));
     };
     
     return (
-        <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+        <CartContext.Provider value={{ cart, cartQuantity, addToCart, removeFromCart}}>
             {children}
     </CartContext.Provider>
   )
