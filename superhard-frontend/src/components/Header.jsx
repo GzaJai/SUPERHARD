4954f012dac9c455d23d1eea8a621115d48b7309
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-10-01 22:04:24 -0300

    implementando el contexto del carrito de compras para mostrar la cantidad del mismo

--- superhard-frontend/src/components/Header.jsx
+++ superhard-frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
-// src/components/Header.jsx
-import React, { useState } from "react";
+import { useState, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import CartContext from "../context/CartContext";
 
 export default function Header() {
   const [search, setSearch] = useState("");
   const categories = ["Procesadores", "Placas de video", "Memorias RAM", "Periféricos", "Gabinetes", "Notebooks"];
+  const { cartQuantity } = useContext(CartContext);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -63,7 +64,7 @@ export default function Header() {
             </Link>
             <Link to={"/shopping-cart"} className="flex px-3 py-1 rounded hover:bg-orange-500 gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" width={25} height={25} fill={"#FFFFFF"} viewBox="0 0 24 24">{/* Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free */}<path d="M10.5 18a1.5 1.5 0 1 0 0 3 1.5 1.5 0 1 0 0-3M17.5 18a1.5 1.5 0 1 0 0 3 1.5 1.5 0 1 0 0-3M8.82 15.77c.31.75 1.04 1.23 1.85 1.23h6.18c.79 0 1.51-.47 1.83-1.2l3.24-7.4c.14-.31.11-.67-.08-.95A1 1 0 0 0 21 7H7.33L5.92 3.62C5.76 3.25 5.4 3 5 3H2v2h2.33z"></path></svg>
-              Carrito (0)
+              Carrito ({ cartQuantity })
             </Link>
           </div>
         </div>
