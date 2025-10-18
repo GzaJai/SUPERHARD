dabbb4040059db60c5f95b9713d445a10c992378
AgustinGenem <Genemagustin@gmail.com>
2025-10-17 21:21:49 -0300

    arreglo scroll horizontal

--- superhard-frontend/src/layouts/PublicLayout.jsx
+++ superhard-frontend/src/layouts/PublicLayout.jsx
@@ -4,9 +4,11 @@ import { Outlet } from "react-router-dom";
 
 export default function PublicLayout({ user, setUser }) {
   return (
-    <div className="flex flex-col min-h-screen bg-neutral-900 text-white">
+    <div className="flex flex-col min-h-screen bg-neutral-900 text-white overflow-x-hidden">
       <Header user={user} setUser={setUser} />
+      <main className="flex-grow">
         <Outlet /> {/* Renderiza las rutas hijas aquí */}
+      </main>
       <Footer />
     </div>
   );
